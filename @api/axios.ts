708f94e5dbf23caa5f21cde52f047caacf5d7279
apple <apple@apples-MacBook-Pro.local>
2022-12-01 09:41:06 +0500

    Api Url Chagne

--- @api/axios.ts
+++ @api/axios.ts
@@ -3,7 +3,7 @@ import { StorageHelper } from "../@helpers";
 
 export const axiosInstance = axios.create({
   baseURL: "http://localhost:3000/api/",
-  // baseURL: "https://meet-your-needs-api.herokuapp.com/api/",
+  // baseURL: "https://fair-gray-rhinoceros-sock.cyclic.app/api/",
 });
 const ResponseInterceptor = (response: AxiosResponse) => {
   return response;
